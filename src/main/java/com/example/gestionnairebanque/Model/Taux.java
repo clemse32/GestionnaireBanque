5cf4d3b68295401f6671f3c099e242c6b34aaa9a
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-03-31 14:41:07 +0200

    Test Taux false

--- src/main/java/com/example/gestionnairebanque/Model/Taux.java
+++ src/main/java/com/example/gestionnairebanque/Model/Taux.java
@@ -1,4 +1,7 @@
 package com.example.gestionnairebanque.Model;
 
+
+
 public class Taux {
+
 }
--- src/main/java/com/example/gestionnairebanque/Test/TauxTest.java
+++ src/main/java/com/example/gestionnairebanque/Test/TauxTest.java
@@ -1,4 +1,14 @@
 package com.example.gestionnairebanque.Test;
 
+import com.example.gestionnairebanque.Model.Taux;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TauxTest {
+    @Test
+    public void testInitTaux() {
+        Taux taux = new Taux("Taux 1", 0d, 1000d, 0.01d);
+
+    }
 }
