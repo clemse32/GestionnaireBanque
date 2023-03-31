96c8befaba85aceb56c32c6850b231cb2897cc7d
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-03-31 13:50:30 +0200

    Mise en forme terminé

--- src/main/java/com/example/gestionnairebanque/GestionnaireBanque.java
+++ src/main/java/com/example/gestionnairebanque/GestionnaireBanque.java
@@ -3,6 +3,8 @@ package com.example.gestionnairebanque;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +14,10 @@ public class GestionnaireBanque extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GestionnaireBanque.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Gestion Bancaire ");
+        scene.getRoot().setStyle("-fx-background-color: LIGHTGRAY;");
+        stage.setResizable(false);
+        stage.getIcons().add(new Image("file:images/Logo.png"));
         stage.setScene(scene);
         stage.show();
     }
