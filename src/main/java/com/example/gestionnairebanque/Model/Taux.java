72f4a37729d54eb996262c7791ed9ce383b37e8d
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-03-31 14:43:35 +0200

    Test Taux passe car problème vient qu'il n'y avait pas de constructeur

--- src/main/java/com/example/gestionnairebanque/Model/Taux.java
+++ src/main/java/com/example/gestionnairebanque/Model/Taux.java
@@ -3,5 +3,15 @@ package com.example.gestionnairebanque.Model;
 
 
 public class Taux {
+    private String nom;
+    private double seuilInf;
+    private double seuilSup;
+    private double taux;
 
+    public Taux(String nom, double seuilInf, double seuilSup, double taux) {
+        this.nom = nom;
+        this.seuilInf = seuilInf;
+        this.seuilSup = seuilSup;
+        this.taux = taux;
+    }
 }
