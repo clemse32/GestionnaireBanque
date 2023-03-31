f3d6b208b8ebedb6c3f6c593ab55bc1f2af8c8f9
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-03-31 15:28:09 +0200

    Test testChargementTauxDepuisFichier ne marche pas

--- src/main/java/com/example/gestionnairebanque/Test/GestionnaireBancaireTest.java
+++ src/main/java/com/example/gestionnairebanque/Test/GestionnaireBancaireTest.java
@@ -1,8 +1,16 @@
 package com.example.gestionnairebanque.Test;
 
 import com.example.gestionnairebanque.Model.GestionnaireBancaire;
+import com.example.gestionnairebanque.Model.Taux;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -20,5 +28,37 @@ public class GestionnaireBancaireTest {
         assertNotNull(gestionnaireBancaire.getTransactions());
         assertEquals(0, gestionnaireBancaire.getTransactions().size());
     }
+
+    @Test
+    public void testInitialisationListeTaux() {
+        assertNotNull(gestionnaireBancaire.getTaux());
+        assertEquals(0, gestionnaireBancaire.getTaux().size());
+    }
+
+    @Test
+    public void testInitialisationSolde() {
+        assertEquals(0.0, gestionnaireBancaire.getSolde(), 0.0);
+    }
+
+
+    @Test
+    public void testChargementTauxDepuisFichier() throws IOException {
+        // Création du fichier temporaire avec des données de test
+        Path path = Paths.get(FILE_PATH);
+        Files.createDirectories(path.getParent());
+        Files.write(path, "CREDIT;0.01\nDEBIT;0.02".getBytes());
+
+        gestionnaireBancaire.chargerTauxDepuisFichier(FILE_PATH);
+
+        List<Taux> tauxAttendus = new ArrayList<>();
+        tauxAttendus.add(new Taux(TypeTransaction.CREDIT, 0.01));
+        tauxAttendus.add(new Taux(TypeTransaction.DEBIT, 0.02));
+
+        assertEquals(tauxAttendus, gestionnaireBancaire.getTaux());
+
+        // Suppression du fichier temporaire
+        Files.deleteIfExists(path);
+    }
+
 
 }
