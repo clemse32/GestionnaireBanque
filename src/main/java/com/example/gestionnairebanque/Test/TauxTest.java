786341b335a24e8665918c04c7b12fb0db2b4d47
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-03-31 14:47:31 +0200

    Tous les test passe pour la classe taux

--- src/main/java/com/example/gestionnairebanque/Test/TauxTest.java
+++ src/main/java/com/example/gestionnairebanque/Test/TauxTest.java
@@ -12,4 +12,23 @@ public class TauxTest {
 
     }
 
+    @Test
+    public void testSetSeuilInf() {
+        Taux taux = new Taux("Taux 1", 0d, 1000d, 0.01d);
+        taux.setSeuilInf(100d);
+        assertEquals(100d, taux.getSeuilInf(), 0.01d);
+    }
+
+    @Test
+    public void testSetSeuilSup() {
+        Taux taux = new Taux("Taux 1", 0d, 1000d, 0.01d);
+        taux.setSeuilSup(500d);
+        assertEquals(500d, taux.getSeuilSup(), 0.01d);
+    }
+    @Test
+    public void testSetTaux() {
+        Taux taux = new Taux("Taux 1", 0d, 1000d, 0.01d);
+        taux.setTaux(0.02d);
+        assertEquals(0.02d, taux.getTaux(), 0.01d);
+    }
 }
