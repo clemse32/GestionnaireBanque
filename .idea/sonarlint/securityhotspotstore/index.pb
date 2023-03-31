7c8344642674bd9a82668b2fefeec61d559a0cc1
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-03-31 14:35:12 +0200

    Test pour la classe Transaction sont passés

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,5 @@
 
 q
 Asrc/main/resources/com/example/gestionnairebanque/hello-view.fxml,f\c\fca959bee5e367c785c85b0953abd45b90d9bd1c
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/com/example/gestionnairebanque/Test/TransactionTest.java
+++ src/main/java/com/example/gestionnairebanque/Test/TransactionTest.java
@@ -15,5 +15,25 @@ public class TransactionTest {
         assertEquals(255d, transaction.getMontant(), 0.01d);
     }
 
+    @Test
+    public void testSetNom() {
+        Transaction transaction = new Transaction("nom", 'c', 255d);
+        transaction.setNom("nouveau_nom");
+        assertEquals("nouveau_nom", transaction.getNom());
+    }
+
+    @Test
+    public void testSetType() {
+        Transaction transaction = new Transaction("nom", 'c', 255d);
+        transaction.setType('d');
+        assertEquals('d', transaction.getType());
+    }
+
+    @Test
+    public void testSetMontant() {
+        Transaction transaction = new Transaction("nom", 'c', 255d);
+        transaction.setMontant(300d);
+        assertEquals(300d, transaction.getMontant(), 0.01d);
+    }
 
 }
